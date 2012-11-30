78fc22643a2a65ac16945bac61c6ae5f20a68815
Reini <reini@dreiwerken.de>
2012-11-30 11:04:50 +0100

    Anpassungen MandyLane

--- MandyLane/Autocompleter.php
+++ MandyLane/Autocompleter.php
@@ -10,14 +10,18 @@ class Autocompleter extends \Templates\Html\Input
 	protected $autocompleterSource;
 
 	/**
-	 * @param $label
-	 * @param array $value
+	 * @param string $name
+	 * @param string $label
+	 * @param string $valueSearchField
+	 * @param bool $valueDataValue
 	 * @param bool $required
 	 * @param array $classOrAttributes
+	 * @param bool $showDataField
+	 * @param string $dataValue
 	 */
-	public function __construct($name, $label, $value, $value2, $required=false, $classOrAttributes = array(), $showSecondField = true, $dataValue = 'id')
+	public function __construct($name, $label, $valueSearchField, $valueDataValue, $required=false, $classOrAttributes = array(), $showDataField = true, $dataValue = 'id')
 	{
-		parent::__construct($name, $value, $label, $required, $classOrAttributes);
+		parent::__construct($name, $valueSearchField, $label, $required, $classOrAttributes);
 
 		$this->addClass('autofocus');
 		$this->addAttribute('data-autocomplete-rel', $name);
@@ -27,19 +31,19 @@ class Autocompleter extends \Templates\Html\Input
 		$this->addAttribute('aria-autocomplete', 'list');
 		$this->addAttribute('aria-haspopup', 'true');
 
-		if($showSecondField)
+		if($showDataField)
 		{
-			$autocompleterField2 =  new \Templates\Html\Input($name.'_'.$dataValue,  $value2, $label.' '.strtoupper($dataValue), $required,'text');
+			$autocompleterDataField =  new \Templates\Html\Input($name.'_'.$dataValue,  $valueDataValue, $label.' '.strtoupper($dataValue), $required,'text');
 		}
 		else
 		{
-			$autocompleterField2 =  new \Templates\Html\Input\Hidden($name.'_'.$dataValue,  $value2, $label.' '.strtoupper($dataValue), $required,'text');
+			$autocompleterDataField =  new \Templates\Html\Input\Hidden($name.'_'.$dataValue,  $valueDataValue, $label.' '.strtoupper($dataValue), $required,'text');
 		}
 
-		$autocompleterField2->addAttribute('data-autocomplete-rel', $name);
-		$autocompleterField2->addAttribute('data-autocomplete-data',$dataValue);
+		$autocompleterDataField->addAttribute('data-autocomplete-rel', $name);
+		$autocompleterDataField->addAttribute('data-autocomplete-data',$dataValue);
 
-		$this->append($autocompleterField2);
+		$this->append($autocompleterDataField);
 
 	}
 
