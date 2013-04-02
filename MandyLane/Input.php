cad02976c51d6d237b0e36d7ada95cbe7f4fb915
Reini <reini@dreiwerken.de>
2013-04-02 14:48:43 +0200

    Kommentare angepasst

--- MandyLane/Input.php
+++ MandyLane/Input.php
@@ -2,9 +2,15 @@
 
 namespace Templates\MandyLane;
 
+/**
+ * Class Input
+ * @package Templates\MandyLane
+ */
 class Input extends \Templates\Html\Input
 {
-
+	/**
+	 * @param bool $value
+	 */
 	public function setAutofocus($value = true)
 	{
 		if($value)
