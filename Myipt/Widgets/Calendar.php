3680b6bba2b6d06412ff71e016f24ee8f4c89715
Alex <alex@dreiwerken.de>
2013-05-21 15:35:56 +0200

    Notficiations

---
+++ Myipt/Widgets/Calendar.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Templates\Myipt\Widgets;
+
+
+class Calendar extends \Templates\Myipt\Widget
+{
+
+	protected $calendar;
+	protected $today;
+	protected $lastday;
+	protected $first;
+	protected $last;
+
+	/**
+	 * @param string $headerText
+	 * @param array $value
+	 * @param bool $style2
+	 * @param array $classOrAttributes
+	 * @param bool $showhidden
+	 */
+	public function __construct($view, $startDate = null)
+	{
+		parent::__construct("Kalender", '', "colHalf calendar");
+
+		$this->setView($view);
+
+		$this->today = new \DateTime();
+		if (!is_null($startDate)){
+			if (!($startDate instanceof \DateTime)){
+				$startDate = new \DateTime($startDate);
+			}
+			$this->today = $startDate;
+		}
+
+		$this->initDays();
+	}
+
+	public function setTerminCount(\DateTime $date, $count){
+		$this->calendar[$date->format("W")][$date->format("d")] = $count;
+	}
+
+	private function initDays(){
+		$today = $this->today;
+		$first = new \DateTime( $today->format("Y-m-1") );
+		$this->first = $first;
+		$last = clone($today);
+		$last->add(new \DateInterval("P1M"));
+		$last->setDate($last->format("Y"), $last->format("m"), 1);
+		$last->setTime(0,0,0);
+		$last->sub(new \DateInterval("P0DT1S"));
+		$this->lastday = $last->format('j');
+		$this->last = $last;
+
+		$cal = array();
+		$temp = clone($first);
+		for ($i = $first->format("W"); $i <= $last->format("W"); $i++){
+			$cal[$i] = array();
+		}
+
+		for ($i = $temp->format("j"); $i <= $last->format("j"); $i++){
+			$cal[$temp->format("W")][($temp->format("N") - 1)] = $temp->format("d");
+			$temp->add(new \DateInterval("P1D"));
+		}
+
+		if (count($cal[$first->format("W")]) < 7){
+			$temp = clone($first);
+			$diff = 7 - count($cal[$first->format("W")]);
+
+			$temp->sub(new \DateInterval("P{$diff}D"));
+
+			for ($i = 0; $i < $diff; $i++){
+				$cal[$first->format("W")][$i] = $temp->format("d");
+				$temp->add(new \DateInterval("P1D"));
+			}
+
+			ksort($cal[$first->format("W")]);
+		}
+
+		if (count($cal[$last->format("W")]) < 7){
+			$temp = clone($last);
+			$diff = 7 - count($cal[$last->format("W")]);
+			$temp->add(new \DateInterval("P1D"));
+
+			for ($i = $last->format("N"); $i < 7; $i++){
+				$cal[$last->format("W")][$i] = $temp->format("d");
+				$temp->add(new \DateInterval("P1D"));
+			}
+
+			ksort($cal[$last->format("W")]);
+		}
+
+		foreach ($cal as $nr => $week){
+			$cal[$nr] = array_flip($week);
+			$cal[$nr] = array_map(function($el){return 0;}, $cal[$nr]);
+		}
+
+		$this->calendar = $cal;
+	}
+
+	public function firstDayOfMonth(){
+		return $this->first;
+	}
+
+	public function lastDayOfMonth(){
+		return $this->last;
+	}
+
+	public function toString(){
+
+		$head = new \Templates\Html\Tag("h2");
+		$this->append($head);
+		$head->append(new \Templates\Myipt\Iconanchor("#", "prev"));
+		$head->append($this->today->format("M"));
+		$head->append(new \Templates\Myipt\Iconanchor("#", "next", null, "fRight"));
+
+
+		$list = new \Templates\Myipt\UnsortedList();
+		$this->append($list);
+
+		$counter = 1;
+		$count = count($this->calendar);
+		$class = 'inactive';
+
+		$weeklist = new \Templates\Myipt\UnsortedList('', array(), 'week');
+		$list->append($weeklist, 'header');
+		$weeklist->append("Mo");
+		$weeklist->append("Di");
+		$weeklist->append("Mi");
+		$weeklist->append("Do");
+		$weeklist->append("Fr");
+		$weeklist->append("Sa");
+		$weeklist->append("So");
+
+		foreach($this->calendar as $wnr => $week){
+
+			$weeklist = new \Templates\Myipt\UnsortedList('', array(), 'week');
+			$list->append($weeklist);
+
+			foreach ($week as $day => $c){
+				if (intval($day) == 1 && $class == "inactive"){
+					$class = "active";
+				} else if (intval($day) == 1 && $class == "active"){
+					$class = "inactive";
+				}
+
+				$temp = $class;
+
+				if ($c > 0){
+					$temp .= ' available';
+				}
+
+				if (intval($day) == intval($this->today->format("j"))){
+					$temp .= ' current';
+				}
+
+				$anchor = new \Templates\Html\Anchor("#", $day);
+				$anchor->addAttribute("data-rel", "{$wnr}-{$day}");
+				$weeklist->append($anchor, $temp);
+
+
+
+			}
+
+			$counter++;
+		}
+
+
+
+		return parent::toString();
+	}
+}
---
+++ Myipt/Widgets/Notifications.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Templates\Myipt\Widgets;
+
+
+class Notifications extends \Templates\Myipt\Widget
+{
+
+	/**
+	 * @param string $headerText
+	 * @param array $value
+	 * @param bool $style2
+	 * @param array $classOrAttributes
+	 * @param bool $showhidden
+	 */
+	public function __construct($view, $headline = null, $notifications = array(), $limit = false, $moreUrl = null, $size = 'col2Third')
+	{
+		parent::__construct($headline, '', "{$size} notifies flow");
+
+		$this->setView($view);
+
+		$list = new \Templates\Myipt\UnsortedList();
+
+		$counter = 0;
+		foreach ($notifications as $noti){
+			if ($limit && $counter >= $limit){
+				break;
+			}
+
+			$class = '';
+			$icon = '';
+			if ($noti->getNotificationposition() && $noti->getNotificationposition()->getAlert() == 1){
+				$class = 'alert';
+				$icon = 'white';
+			}
+
+			$list->append(
+				sprintf(
+					"%s %s",
+					new \Templates\Html\Anchor(
+						$this->view->url(array('module'=>'notifications', 'controller'=>'index', 'action'=>'redirect', 'id' => $noti->getId())),
+						substr($noti->getText(), 0, 100)
+					),
+					new \Templates\Myipt\Iconanchor(
+						$this->view->url(array('module'=>'notifications', 'controller'=>'index', 'action'=>'redirect', 'id' => $noti->getId())),
+						"next big {$icon}"
+					)
+				),
+				$class
+			);
+			$counter++;
+		}
+
+
+		if ($limit && count($notifications) > $limit){
+			$url = $this->view->url(array('module'=>'notifications', 'controller'=>'index', 'action'=>'index'));
+			if (!is_null($moreUrl)){
+				$url = $moreUrl;
+			}
+
+			$anchor = new \Templates\Html\Anchor($url, "mehr");
+			$this->setFooter($anchor, 'notifi');
+		}
+
+		$this->append($list);
+
+
+
+
+
+	}
+
+}
