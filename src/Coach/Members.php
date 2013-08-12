11566ad725500824fe192418adb1f130979e8b76
Reini <reini@dreiwerken.de>
2013-08-12 10:30:23 +0200

    Code Doc

--- src/Coach/Members.php
+++ src/Coach/Members.php
@@ -5,18 +5,40 @@ namespace Templates\Coach;
 
 class Members extends \Templates\Html\Tag
 {
-
+	/**
+	 * @var null
+	 */
 	protected $view = null;
+
+	/**
+	 * @var array
+	 */
 	protected $members = array();
 
+	/**
+	 * @var array|bool
+	 */
 	protected $selectLink = false;
+
+	/**
+	 * @var bool
+	 */
 	protected $disableControls = false;
 
+	/**
+	 * @param array       $members
+	 * @param array       $classOrAttributes
+	 * @param bool|array  $selectLink
+	 * @param bool        $disableControls
+	 */
 	public function __construct(array $members, $classOrAttributes = array(), $selectLink = false, $disableControls = false)
 	{
-		if (is_array($classOrAttributes)){
+		if (is_array($classOrAttributes))
+		{
 			$classOrAttributes[] = "memberList";
-		} else {
+		}
+		else
+		{
 			$classOrAttributes .= " memberList";
 		}
 
@@ -27,23 +49,35 @@ class Members extends \Templates\Html\Tag
 		$this->disableControls = $disableControls;
 	}
 
+	/**
+	 * @param \Core\View $view
+	 * @return void
+	 */
 	public function setView($view)
 	{
 		$this->view = $view;
 	}
 
+	/**
+	 * @return string
+	 */
+	public function toString()
+	{
 
-	public function toString(){
-
-		foreach ($this->members as $u){
-			if ($this->selectLink){
+		foreach ($this->members as $u)
+		{
+			if ($this->selectLink)
+			{
 				$this->selectLink["userid"] = $u->getId();
 				$m = new \Templates\Coach\Member($u, $this->view->url($this->selectLink));
-			} else {
+			}
+			else
+			{
 				$m = new \Templates\Coach\Member($u);
 			}
 
-			if ($this->disableControls){
+			if ($this->disableControls)
+			{
 				$m->disableControls();
 			}
 
