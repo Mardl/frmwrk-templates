388900c5a3f533a10107f495e52428d2720293b6
Reini <reini@dreiwerken.de>
2013-09-03 11:08:46 +0200

    Checkstyle

--- src/Myipt/Section.php
+++ src/Myipt/Section.php
@@ -4,6 +4,13 @@ namespace Templates\Myipt;
 
 use	Templates\Html\Tag;
 
+/**
+ * Class Section
+ *
+ * @category Lifemeter
+ * @package  Templates\Myipt
+ * @author   Reinhard Hampl <reini@dreiwerken.de>
+ */
 class Section extends Tag
 {
 	/**
@@ -14,16 +21,18 @@ class Section extends Tag
 
 	/**
 	 * @param string $headerText
-	 * @param array $value
-	 * @param bool $style2
-	 * @param array $classOrAttributes
-	 * @param bool $showhidden
+	 * @param array  $value
+	 * @param array  $classOrAttributes
+	 * @param bool   $showhidden
+	 * @internal param bool $style2
+	 * @return \Templates\Myipt\Section
 	 */
 	public function __construct($headerText='', $value=array(), $classOrAttributes = array(), $showhidden = false)
 	{
 		parent::__construct('div', '', 'section');
 
-		if (!empty($headerText)){
+		if (!empty($headerText))
+		{
 			$h2 = new Tag('h2');
 			$span = new Tag('span', $headerText);
 			$h2->append($span);
@@ -49,20 +58,32 @@ class Section extends Tag
 	 */
 	protected function initContent()
 	{
-		$this->content = new Tag('div','','content');
+		$this->content = new Tag('div', '', 'content');
 		parent::append($this->content);
 	}
 
+	/**
+	 * @param mixed $value
+	 * @return void
+	 */
 	public function append($value)
 	{
 		$this->content->append($value);
 	}
 
+	/**
+	 * @param mixed $value
+	 * @return void
+	 */
 	public function prepend($value)
 	{
 		$this->content->prepend($value);
 	}
 
+	/**
+	 * @param bool $sortable
+	 * @return void
+	 */
 	public function setContentSortable($sortable = true)
 	{
 		if($sortable)
@@ -75,9 +96,12 @@ class Section extends Tag
 	}
 
 
+	/**
+	 * @return string
+	 */
 	public function toString()
 	{
-		$br = new \Templates\Html\Tag("br",'','clear');
+		$br = new \Templates\Html\Tag("br", '', 'clear');
 		$br->forceClose = false;
 
 		$this->append($br);
