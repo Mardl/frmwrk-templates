ba3ebfc8779157f0a5cc962e2d25ca756c0bf431
Cindy Paulitz <cindy@dreiwerken.de>
2013-10-15 16:48:55 +0200

    toogle nur, wenn auch was zum toggeln da ist

--- src/Srabon/ToggleMenu.php
+++ src/Srabon/ToggleMenu.php
@@ -2,67 +2,113 @@
 
 namespace Templates\Srabon;
 
-use	Templates\Html\Tag;
+use    Templates\Html\Tag;
 
+/**
+ * Class ToggleMenu
+ *
+ * @category Thomann
+ * @package  Templates\Srabon
+ * @author   Cindy Paulitz <cindy@dreiwerken.de>
+ */
 class ToggleMenu extends Tag
 {
+
+	/**
+	 * @var bool
+	 */
 	private $right = true;
+	/**
+	 * @var \Templates\Html\Tag
+	 */
 	private $button;
+	/**
+	 * @var \Templates\Html\Tag
+	 */
 	private $menu;
 
 	/**
 	 * @param string $title
-	 * @param array $menu
-	 * @param array $classOrAttributes
+	 * @param array  $menu
+	 * @param array  $classOrAttributes
 	 */
-	public function __construct($title,$menu=array(),$classOrAttributes = array())
+	public function __construct($title, $menu = array(), $classOrAttributes = array())
 	{
-		parent::__construct('div','',$classOrAttributes);
+		parent::__construct('div', '', $classOrAttributes);
 		$this->addClass('btn-group');
 
-		$this->button = new Tag('button','','btn dropdown-toggle');
+		$this->button = new Tag('button', '', 'btn dropdown-toggle');
 		$this->setButton($title);
-		$this->button->addAttribute('data-toggle','dropdown');
+		$this->button->addAttribute('data-toggle', 'dropdown');
 		parent::append($this->button);
 
-		$this->menu = new Tag('ul','','dropdown-menu');
+		$this->menu = new Tag('ul', '', 'dropdown-menu');
 		if (!empty($menu) && is_array($menu))
 		{
-			foreach($menu as $values)
+			foreach ($menu as $values)
 			{
-				 $this->append($values);
+				$this->append($values);
 			}
 		}
 
 		parent::append($this->menu);
 	}
 
-	public function setButton($title,$iconclass="icon-cog")
+	/**
+	 * @param string $title
+	 * @param string $iconclass
+	 * @return void
+	 */
+	public function setButton($title, $iconclass = "icon-cog")
 	{
-		$this->button->set('<i class="'.$iconclass.'"></i> '.$title.' <span class="caret"></span>');
+		$this->button->set('<i class="' . $iconclass . '"></i> ' . $title . ' <span class="caret"></span>');
 	}
 
+	/**
+	 * @return void
+	 */
 	public function right()
 	{
 		$this->right = true;
 	}
+
+	/**
+	 * @return void
+	 */
 	public function left()
 	{
 		$this->right = false;
 	}
 
+	/**
+	 * @param mixed $value
+	 * @return void
+	 */
 	public function append($value)
 	{
-		$li = new Tag('li',$value);
-		$this->menu->append($li);
+		if ($value != '')
+		{
+			$li = new Tag('li', $value);
+			$this->menu->append($li);
+		}
 	}
 
+	/**
+	 * @param mixed $value
+	 * @return void
+	 */
 	public function prepend($value)
 	{
-		$li = new Tag('li',$value);
-		$this->menu->append($li);
+		if ($value != '')
+		{
+			$li = new Tag('li', $value);
+			$this->menu->append($li);
+		}
 	}
 
+	/**
+	 * @return string
+	 */
 	public function toString()
 	{
 		$this->right ? $this->addClass('pull-right') : $this->addClass('pull-left');
@@ -77,6 +123,11 @@ class ToggleMenu extends Tag
                       </div>
 		';
 		*/
+
+		if (empty($this->menu->tagInner))
+		{
+			return '';
+		}
 
 		return parent::toString();
 	}
