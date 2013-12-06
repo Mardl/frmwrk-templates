955384fc69ac22fa5ac78c1b6011ed73d2983170
Reini <reini@dreiwerken.de>
2013-12-06 11:47:48 +0100

    Umbauen der MandyLane Page

--- src/MandyLane/Page.php
+++ src/MandyLane/Page.php
@@ -2,49 +2,114 @@
 
 namespace Templates\MandyLane;
 
-use Core\SystemMessages;
-
+/**
+ * Class Page
+ *
+ * @category Lifemeter
+ * @package  Templates\MandyLane
+ * @author   Reinhard Hampl <reini@dreiwerken.de>
+ */
 class Page extends \Templates\Html\Tag
 {
-	/**
-	 * @var Tag
-	 */
-	protected $header = null;
 
 	/**
-	 * @var Tag
+	 * @var \Core\View|null
 	 */
-	protected $contentContainer = null;
+	protected $view = null;
 
 	/**
-	 * @var null
+	 * @var string
 	 */
-	protected $content = null;
+	protected $headertext = '';
 
 	/**
-	 * @var \Core\HTMLHelper|null
+	 * @var array
 	 */
-	protected $htmlHelper = null;
+	protected $dataContent = array();
 
 	/**
-	 * @param string $headerText
-	 * @param array $classOrAttributes
+	 * @param string     $headerText
+	 * @param \Core\View $view
+	 * @param array      $dataContent
+	 * @param array      $classOrAttributes
 	 */
-	public function __construct($headerText, $dataContent = array(), $classOrAttributes = array())
+	public function __construct($headerText, $view, $dataContent = array(), $classOrAttributes = array())
 	{
 		parent::__construct('div', '', $classOrAttributes);
 
-		$this->htmlHelper = new \Core\HTMLHelper();
+		$this->setHeadertext($headerText);
+		$this->setDataContent($dataContent);
+		$this->setView($view);
+	}
+
+	/**
+	 * @param null $view
+	 */
+	public function setView($view)
+	{
+		$this->view = $view;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function getView()
+	{
+		return $this->view;
+	}
+
+	/**
+	 * @param string $headertext
+	 */
+	public function setHeadertext($headertext)
+	{
+		$this->headertext = $headertext;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHeadertext()
+	{
+		return $this->headertext;
+	}
+
+	/**
+	 * @param null $dataContent
+	 */
+	public function setDataContent($dataContent)
+	{
+		$this->dataContent = $dataContent;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function getDataContent()
+	{
+		return $this->dataContent;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function toString()
+	{
+		$view = $this->getView();
 
 		$this->addClass('maincontent');
 
-
-		$header = new \Templates\Html\Tag('h1', array($headerText), array('class' => 'pageTitle'));
+		$header = new \Templates\Html\Tag('h1', array($this->getHeadertext()), array('class' => 'pageTitle'));
 		$header->addClass('pageTitle');
-		$this->contentContainer = new \Templates\Html\Tag('div');
-		$this->contentContainer->addClass('left');
-		$this->contentContainer->append(array($header, $dataContent));
-		$this->append(array($this->htmlHelper->viewBreadcrumbs(), $this->contentContainer));
+
+		$contentContainer = new \Templates\Html\Tag('div');
+		$contentContainer->addClass('left');
+		$contentContainer->append(array($header, $this->getDataContent()));
+
+		$this->append(array($view->html->viewBreadcrumbs(), $contentContainer));
+
+		return parent::toString();
 	}
 
 }
