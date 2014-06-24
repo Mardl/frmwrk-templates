8cd44ddf2f9ff3fe3d0c9acf140a4ee87e3193fd
Martin Eisenführer <martin@dreiwerken.de>
2014-06-24 20:01:08 +0200

    Ausrichtung Tabs implementiert

--- src/Srabon/Tabs.php
+++ src/Srabon/Tabs.php
@@ -6,6 +6,12 @@ use Templates\Html\Tag;
 
 class Tabs extends \Templates\Srabon\Widget
 {
+
+	const ORIENTATION_TOP = 0;
+	const ORIENTATION_LEFT = 1;
+	const ORIENTATION_RIGHT = 2;
+	const ORIENTATION_BOTTOM = 3;
+
 	/**
 	 * @var array
 	 */
@@ -19,6 +25,13 @@ class Tabs extends \Templates\Srabon\Widget
 	 * @var \Templates\Html\Tag
 	 */
 	protected $navigation = null;
+
+	/**
+	 * @var \Templates\Html\Tag
+	 */
+	private $container = null;
+
+	private $orientation = self::ORIENTATION_TOP;
 
 	/**
 	 * @var int
@@ -135,13 +148,31 @@ class Tabs extends \Templates\Srabon\Widget
 
 
 		$this->content = new Tag('div','','tab-content well');
-		$wrap0 = new Tag('div',array($this->navigation, $this->content),'');
 		$this->content->addClass('white-box');
 
-		$wrap1 = new Tag('div',$wrap0,'widget-box tabbable');
-		$wrap2 = new Tag('div',$wrap1,'widget-content');
+		$wrap0 = array($this->navigation, $this->content);
+
+		$this->container = new Tag('div',$wrap0,'widget-box tabbable');
+		$wrap2 = new Tag('div',$this->container,'widget-content');
 
 		Tag::append($wrap2);
+	}
+
+	public function setTabsLeft()
+	{
+		$this->orientation = self::ORIENTATION_LEFT;
+	}
+	public function setTabsRight()
+	{
+		$this->orientation = self::ORIENTATION_RIGHT;
+	}
+	public function setTabsTop()
+	{
+		$this->orientation = self::ORIENTATION_TOP;
+	}
+	public function setTabsBottom()
+	{
+		$this->orientation = self::ORIENTATION_BOTTOM;
 	}
 
 	public function toString()
@@ -154,6 +185,17 @@ class Tabs extends \Templates\Srabon\Widget
 		if ($this->activePage < 0)
 		{
 			$this->setActive(0);
+		}
+
+		switch ($this->orientation)
+		{
+			case self::ORIENTATION_LEFT : $this->container->addClass('tabs-left');
+				break;
+			case self::ORIENTATION_RIGHT : $this->container->addClass('tabs-right');
+				break;
+			case self::ORIENTATION_BOTTOM : $this->container->addClass('tabs-below');
+				$this->container->set(array($this->content, $this->navigation));
+				break;
 		}
 
 		return parent::toString();
